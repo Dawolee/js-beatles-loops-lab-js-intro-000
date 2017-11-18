5b11a0a2c83b0595de395527b542e450d59675be
Da Woon Lee <daniel.lee9415@gmail.com>
2017-11-18 00:09:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(arr) {
   var i = arr.length;
   while (i > -1) {
-    arr[i].concat("!!!");
+    arr[i] = arr[i] + "!!!";
   }
   return arr;
 }
