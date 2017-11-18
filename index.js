7fc8bff8729628aaa1391860542f5bde7a3981ac
Da Woon Lee <daniel.lee9415@gmail.com>
2017-11-18 00:23:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ function johnLennonFacts(arr) {
   }
   return arr;
 }
+
+function iLoveTheBeatles(n) {
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+}
